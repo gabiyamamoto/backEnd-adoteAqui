c5c997bc6771e24535235a34c6238889a0b7db37
gabi emi <gabiemi2021@gmail.com>
2025-11-28 14:23:00 -0300

    Deleta rota do run-seed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 
 import petsRoutes from "./src/routes/petsRoutes.js";
 import tiposRoutes from './src/routes/tiposRoutes.js';
-import { seed } from "./prisma/seed.js";
 
 const app = express();
 app.use(express.json());
@@ -14,22 +13,12 @@ dotenv.config();
 const serverPort = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-    res.send("🚀 Servidor funcionando...");
-});
-
-app.post("/run-seed", async (req, res) => {
-  try {
-    await seed();
-    res.send("Seed executado com sucesso!");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao executar seed");
-  }
+  res.send("🚀 Servidor funcionando...");
 });
 
 app.use("/pets", petsRoutes);
 app.use('/tipos', tiposRoutes);
 
 app.listen(serverPort, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+  console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
 });
